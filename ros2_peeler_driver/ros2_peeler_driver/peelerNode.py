b7ef27fbcf812cb93ad7b1ef1d9359d328e2421a
MDecarabas <codrea.eric@gmail.com>
2022-07-18 15:48:19 -0500

    Updated sealerNode with driver functionality and readme with port setup instructions.

--- ros2_peeler_driver/ros2_peeler_driver/peelerNode.py
+++ ros2_peeler_driver/ros2_peeler_driver/peelerNode.py
@@ -1,6 +1,7 @@
 from .drivers.peeler_client import BROOKS_PEELER_CLIENT
 peeler = BROOKS_PEELER_CLIENT("/dev/ttyUSB0")
 def main():
+    print('Hi from ros2_peeler_driver.')
     peeler.check_status()
 
 if __name__ == '__main__':
--- ros2_sealer_driver/ros2_sealer_driver/sealerNode.py
+++ ros2_sealer_driver/ros2_sealer_driver/sealerNode.py
@@ -1,8 +1,9 @@
 from .drivers.sealer_client import A4S_SEALER_CLIENT
+sealer = A4S_SEALER_CLIENT("/dev/ttyUSB0")
 
 def main():
     print('Hi from ros2_sealer_driver.')
-
+    sealer.reset()
 
 if __name__ == '__main__':
     main()
