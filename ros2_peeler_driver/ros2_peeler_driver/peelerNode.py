5b4df062d8b70f8595f434fe2dc54321f660244c
MDecarabas <codrea.eric@gmail.com>
2022-07-18 17:50:25 -0500

    Implemented switch cases for peeler/seeler nodes.

--- ros2_peeler_driver/ros2_peeler_driver/peelerNode.py
+++ ros2_peeler_driver/ros2_peeler_driver/peelerNode.py
@@ -1,14 +1,16 @@
 from .drivers.peeler_client import BROOKS_PEELER_CLIENT
 peeler = BROOKS_PEELER_CLIENT("/dev/ttyUSB0")
+
 def main():
     # Run commands if manager sends corresponding command
     manager_command = "test_command"
     print('Hi from ros2_peeler_driver.')
 
-    if manager_command == "test_command":
-        peeler.check_status()
-        peeler.check_version()
-        peeler.reset()
+    match manager_command:
+        case "test_command":
+            peeler.check_status()
+            peeler.check_version()
+            peeler.reset()
 
     print(peeler.peeler_output)
 
--- ros2_sealer_driver/ros2_sealer_driver/sealerNode.py
+++ ros2_sealer_driver/ros2_sealer_driver/sealerNode.py
@@ -2,8 +2,12 @@ from .drivers.sealer_client import A4S_SEALER_CLIENT
 sealer = A4S_SEALER_CLIENT("/dev/ttyUSB0")
 
 def main():
+    # Run commands if manager sends corresponding command
+    manager_command = "test_command"
     print('Hi from ros2_sealer_driver.')
-    sealer.reset()
+    match manager_command:
+        case "test_command":
+            sealer.reset()
 
 if __name__ == '__main__':
     main()
