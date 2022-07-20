78d6bfedf582a106a69af03b16107a1b78132223
MDecarabas <codrea.eric@gmail.com>
2022-07-20 11:51:56 -0500

    Minor changes to peelerNode

--- ros2_peeler_driver/ros2_peeler_driver/drivers/peeler_client.py
+++ ros2_peeler_driver/ros2_peeler_driver/drivers/peeler_client.py
@@ -280,7 +280,7 @@ class BROOKS_PEELER_CLIENT():
 if __name__ == "__main__":
 
     dummy_peel = BROOKS_PEELER_CLIENT("/dev/ttyUSB0")
-    dummy_peel.check_status()
-    for i in range(5):
-        print('\n')
-    print(dummy_peel.peeler_output)
+    dummy_peel.peel(1, 2.5)
+    # for i in range(5):
+    #     print('\n')
+    # print(dummy_peel.peeler_output)
